236727d2f86320760a3b9fa92d456193b5da3f52
Yamato Nakahara <yamrnak@gmail.com>
2022-03-17 14:32:06 -0700

    record all expanded reviews. html tags included in text.

--- metacritic_html_scraper.py
+++ metacritic_html_scraper.py
@@ -12,9 +12,16 @@ def html_scraper():
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
     """     url = input("Enter the Metacritic URL: ") """
     url = 'https://www.metacritic.com/game/playstation-5/elden-ring/user-reviews'
-    page_request = requests.get(url, headers=headers)
 
-    html_page = BeautifulSoup(page_request.text, 'html.parser')
+    driver = webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+    driver.get(url)
+    driver.implicitly_wait(5)
+    button = driver.find_elements(By.XPATH, "//span/span[contains(.,'Expand')]")
+
+    for button in button:
+        button.click()
+
+    html_page = BeautifulSoup(driver.page_source, 'html.parser')
 
     main_review_body = html_page.find("div", class_="body product_reviews")
     review_bodies = main_review_body.find_all("div", class_ = "review_content".split())
@@ -28,12 +35,9 @@ def html_scraper():
     for review in review_bodies:
         date = review.find("div", class_ = "date").string
         score = review.find("div", class_ = "metascore_w").string
-        try:
-            written_review = review.find("span", class_ = "blurb blurb_expanded").string
-        except AttributeError:
-            written_review = review.find("span", class_ = "blurb blurb_expanded")
-            if written_review is None:
-                written_review = review.find("div", class_ = "review_body").find("span")
+        written_review = review.find("span", class_ = "blurb blurb_expanded")
+        if written_review is None:
+            written_review = review.find("div", class_ = "review_body").find("span")
         total_thumbs_ups = review.find("span", class_ = "total_ups").string
         total_thumbs = review.find("span", class_ = "total_thumbs").string
         data.append(['elden-ring', date, score, written_review, total_thumbs_ups, total_thumbs])
