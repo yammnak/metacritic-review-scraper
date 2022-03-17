44d78f4f81c4b70367a9ed5cd76735714989d091
Yamato Nakahara <yamrnak@gmail.com>
2022-03-17 12:51:14 -0700

    Creates dataframe with some of the reviews.

--- metacritic_html_scraper.py
+++ metacritic_html_scraper.py
@@ -1,25 +1,39 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 def html_scraper():
+    data = []
+
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'}
-    url = input("Enter the Metacritic URL: ")
-    page = requests.get(url, headers=headers)
+    """     url = input("Enter the Metacritic URL: ") """
+    page_request = requests.get('https://www.metacritic.com/game/playstation-5/elden-ring/user-reviews', headers=headers)
 
-    test = BeautifulSoup(page.text, 'html.parser')
+    html_page = BeautifulSoup(page_request.text, 'html.parser')
 
-    tag = test.find("div", class_="body product_reviews")
-    asdf = tag.find_all("div", class_ = "review_content".split())
+    main_review_body = html_page.find("div", class_="body product_reviews")
+    review_bodies = main_review_body.find_all("div", class_ = "review_content".split())
 
-# Find number of pages of reviews
+    # Find number of pages of reviews
     try:
-        page_nums = int(test.find("li", class_ = "page last_page").find("a", class_ = "page_num").string)
+        page_nums = int(html_page.find("li", class_ = "page last_page").find("a", class_ = "page_num").string)
     except AttributeError:
         page_nums = 0
 
-    p = 0
+    for review in review_bodies:
+        date = review.find("div", class_ = "date").string
+        score = review.find("div", class_ = "metascore_w").string
+        try:
+            written_review = review.find("span", class_ = "blurb blurb_expanded").string
+        except AttributeError:
+            written_review = review.find("span", class_ = "blurb blurb_expanded")
+            if written_review is None:
+                written_review = review.find("div", class_ = "review_body").find("span")
+        total_thumbs_ups = review.find("span", class_ = "total_ups").string
+        total_thumbs = review.find("span", class_ = "total_thumbs").string
+        data.append(['elden-ring', date, score, written_review, total_thumbs_ups, total_thumbs])
 
-    for i in range(1,len(asdf)):
-        p = p+1
+    df = pd.DataFrame(data, columns=['game', 'date', 'metacritic_score', 'review', 'thumbs_ups', 'total_thumbs'])
+    df.to_csv("output.csv")
 
-    print(p)
+    print(review_bodies[1])
